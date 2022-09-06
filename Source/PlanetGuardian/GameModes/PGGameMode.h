ffdb657fc75e0ad146421be5ea42f164a01f5e95
ongbular <ongbular@gmail.com>
2022-09-06 14:51:13 +0900

    Add Base GameMode

---
+++ Source/PlanetGuardian/GameModes/PGGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PGGameMode.generated.h"
+
+/**
+ * 이 프로젝트의 Base 게임모드 입니다.
+ */
+UCLASS()
+class PLANETGUARDIAN_API APGGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
